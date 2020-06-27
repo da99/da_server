8a568a3357eb251d78ad022c655313b04279a590
da99 <v01@miniuni.com>
2020-06-27 10:55:59 -0500

    Typos.

--- src/da_server/Not_Found.cr
+++ src/da_server/Not_Found.cr
@@ -1,12 +1,15 @@
 
-class Not_Found
-  include HTTP::Handler
-  def initialize
-  end # def
 
-  def call(ctx)
-    ctx.response.status_code = 404
-    ctx.response.content_type = "text/plain"
-    ctx.response.print "404 NOT FOUND: #{ ctx.request.path.inspect  }"
-  end # def
-end # class
+struct DA_Server
+  class Not_Found
+    include HTTP::Handler
+    def initialize
+    end # def
+
+    def call(ctx)
+      ctx.response.status_code = 404
+      ctx.response.content_type = "text/plain"
+      ctx.response.print "404 NOT FOUND: #{ ctx.request.path.inspect  }"
+    end # def
+  end # class
+end # struct
